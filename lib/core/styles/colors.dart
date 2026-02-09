a08193b30b2a0943fcb3bd66a3fec58d9cf55c44
feras.Oz <ferasosama99@gmail.com>
2026-02-09 12:49:06 +0200

    - Creating helper files

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color primary = Color(0xFF1E1E1E);
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFFB0B0B0);
+}
