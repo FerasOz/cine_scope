4303823b8ca04e96ec95808c326858dc0fc2dfcb
feras.Oz <ferasosama99@gmail.com>
2026-02-16 11:56:51 +0200

    _ Creating dio factory file

---
+++ lib/core/networking/dio_factory.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  /// private constructor as I don't want to allow creating an instance of this class
+  DioFactory._();
+
+  static Dio? dio;
+
+  static Future<Dio> getDio() async {
+    Duration timeOut = const Duration(seconds: 30);
+
+    if (dio == null) {
+      dio = Dio();
+      dio!
+        ..options.connectTimeout = timeOut
+        ..options.receiveTimeout = timeOut;
+      await addDioHeaders();
+      addDioInterceptor();
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  static Future<void> addDioHeaders() async {
+    dio?.options.headers = {
+      'Accept': 'application/json',
+      'Authorization':
+          'Bearer MY_API_KEY',
+    };
+  }
+
+  static void addDioInterceptor() {
+    dio?.interceptors.add(
+      PrettyDioLogger(
+        requestBody: true,
+        requestHeader: true,
+        responseHeader: true,
+      ),
+    );
+  }
+}
