b3f262f0f5ab8e973e6d88e136a005d9114f4a7a
feras.Oz <ferasosama99@gmail.com>
2026-02-16 11:58:06 +0200

    - Adding base url & end points

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const trendingMovies = "/trending/movie/day";
+  static const popularMovies = "/movie/popular";
+  static const topRatedMovies = "/movie/top_rated";
+  static const searchMovies = "/search/movie";
+}
