3051606cde41958a9a871ef116122358d9f09126
feras.Oz <ferasosama99@gmail.com>
2026-02-19 10:50:28 +0200

    - Adding getit setup method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cine_scope/cine_scope_app.dart';
+import 'package:cine_scope/core/di/dependency_injection.dart';
 import 'package:cine_scope/core/routing/app_routers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -6,5 +7,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await setUpGetIt();
   runApp(CineScopeApp(appRouters: AppRouters()));
 }
