152ee42e79519a9676113fd9b98cb74c0e9c933f
feras.Oz <ferasosama99@gmail.com>
2026-02-22 10:28:46 +0200

    - Adding Casts & Reviews methods

--- lib/features/details/data/repo/movie_details_repo.dart
+++ lib/features/details/data/repo/movie_details_repo.dart
@@ -1,7 +1,9 @@
 import 'package:cine_scope/core/networking/api_error_handler.dart';
 import 'package:cine_scope/core/networking/api_result.dart';
 import 'package:cine_scope/core/networking/api_service.dart';
-import 'package:cine_scope/features/details/data/models/movie_details_model.dart';
+import 'package:cine_scope/features/details/data/models/casts/credits_response_model.dart';
+import 'package:cine_scope/features/details/data/models/details/movie_details_model.dart';
+import 'package:cine_scope/features/details/data/models/review/reviews_response_model.dart';
 
 class MovieDetailsRepo {
   final ApiService _apiService;
@@ -11,6 +13,24 @@ class MovieDetailsRepo {
   Future<ApiResult<MovieDetailsModel>> getMovieDetails(int movieId) async {
     try {
       final response = await _apiService.getMovieDetails(movieId);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ApiErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<ReviewsResponseModel>> getMovieReviews(int movieId) async {
+    try {
+      final response = await _apiService.getMovieReviews(movieId);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ApiErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<CreditsResponseModel>> getMovieCast(int movieId) async {
+    try {
+      final response = await _apiService.getMovieCast(movieId);
       return ApiResult.success(response);
     } catch (error) {
       return ApiResult.failure(ApiErrorHandler.handle(error));
