ca6409a3e38d3cf751ae37b145cb07864254129c
feras.Oz <ferasosama99@gmail.com>
2026-02-27 10:13:40 +0200

    - adding weekly trending movies end point

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -2,6 +2,7 @@ class ApiConstants {
   static const String baseUrl = "https://api.themoviedb.org/3/";
   static const String apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODYzZWUzZDBlY2U4MjBkZmM2N2U5YWYzYjgwZDM4YSIsIm5iZiI6MTc3MDUzNDk4MC45MTM5OTk4LCJzdWIiOiI2OTg4Mzg0NDBiNDVjNTAzNTU4ZmNiZmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WFu-QkJrubPYZyBLCOLxVBF0sOLloA_4zdhsIFIJh1U";
   static const trendingMovies = "/trending/movie/day";
+  static const trendingMoviesWeek = "/trending/movie/week";
   static const popularMovies = "/movie/popular";
   static const topRatedMovies = "/movie/top_rated";
   static const searchMovies = "/search/movie";
