cb48bd7f10d4a4e08afb16960f6710900b07cd0e
feras.Oz <ferasosama99@gmail.com>
2026-02-28 11:28:50 +0200

    - Setting up hive in main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:cine_scope/core/di/dependency_injection.dart';
 import 'package:cine_scope/core/routing/app_routers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Hive.initFlutter();
+  await Hive.openBox<Map>('watchlist');
   await setUpGetIt();
   runApp(CineScopeApp(appRouters: AppRouters()));
 }
