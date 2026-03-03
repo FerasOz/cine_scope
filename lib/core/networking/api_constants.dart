98b90600f13bdb14f13bd07a93899c3ee3a1d6c7
feras.Oz <ferasosama99@gmail.com>
2026-03-03 11:02:26 +0200

    - adding tv end points

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,13 +1,23 @@
 class ApiConstants {
   static const String baseUrl = "https://api.themoviedb.org/3/";
-  static const String apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODYzZWUzZDBlY2U4MjBkZmM2N2U5YWYzYjgwZDM4YSIsIm5iZiI6MTc3MDUzNDk4MC45MTM5OTk4LCJzdWIiOiI2OTg4Mzg0NDBiNDVjNTAzNTU4ZmNiZmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WFu-QkJrubPYZyBLCOLxVBF0sOLloA_4zdhsIFIJh1U";
-  static const trendingMovies = "/trending/movie/day";
-  static const trendingMoviesWeek = "/trending/movie/week";
-  static const popularMovies = "/movie/popular";
-  static const topRatedMovies = "/movie/top_rated";
-  static const searchMovies = "/search/movie";
-  static const genrehMovies = "/discover/movie";
-  static const movieDetails = "movie/{movie_id}";
-  static const movieReviews = "movie/{movie_id}/reviews";
-  static const movieCasts= "movie/{movie_id}/credits";
+  static const String apiToken =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODYzZWUzZDBlY2U4MjBkZmM2N2U5YWYzYjgwZDM4YSIsIm5iZiI6MTc3MDUzNDk4MC45MTM5OTk4LCJzdWIiOiI2OTg4Mzg0NDBiNDVjNTAzNTU4ZmNiZmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.WFu-QkJrubPYZyBLCOLxVBF0sOLloA_4zdhsIFIJh1U";
+
+  /// Trending (movie / tv)
+  static const trending = "/trending/{media_type}/{time_window}";
+
+  /// Popular / Top Rated
+  static const popular = "/{media_type}/popular";
+  static const topRated = "/{media_type}/top_rated";
+
+  /// Details
+  static const details = "/{media_type}/{id}";
+  static const reviews = "/{media_type}/{id}/reviews";
+  static const credits = "/{media_type}/{id}/credits";
+
+  /// Search
+  static const search = "/search/{media_type}";
+
+  /// Discover (genre)
+  static const discover = "/discover/{media_type}";
 }
