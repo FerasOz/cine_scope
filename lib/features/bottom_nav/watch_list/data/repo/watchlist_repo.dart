c4ddcf0437ac65c59159fca48df0ffd98373c67b
feras.Oz <ferasosama99@gmail.com>
2026-03-10 11:31:02 +0200

    - Changing watchlist model name

--- lib/features/bottom_nav/watch_list/data/repo/watchlist_repo.dart
+++ lib/features/bottom_nav/watch_list/data/repo/watchlist_repo.dart
@@ -1,16 +1,16 @@
 import 'package:cine_scope/features/bottom_nav/watch_list/data/local/watchlist_local_data_source.dart';
-import 'package:cine_scope/features/bottom_nav/watch_list/data/models/watchlist_movie.dart';
+import 'package:cine_scope/features/bottom_nav/watch_list/data/models/watchlist_model.dart';
 
 class WatchlistRepo {
   final WatchlistLocalDataSource local;
 
   WatchlistRepo(this.local);
 
-  List<WatchlistMovie> getWatchlist() {
+  List<WatchlistModel> getWatchlist() {
     return local.getMovies();
   }
 
-  Future<void> toggleMovie(WatchlistMovie movie) async {
+  Future<void> toggleMovie(WatchlistModel movie) async {
     final exists = local.isMovieInWatchlist(movie.id);
 
     if (exists) {
