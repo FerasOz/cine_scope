476c6e6f7f85d44e66f7335be622f088cea7b64c
feras.Oz <ferasosama99@gmail.com>
2026-03-16 10:53:21 +0200

    - Adding pagination to search body

--- lib/features/bottom_nav/search/ui/widgets/search_body.dart
+++ lib/features/bottom_nav/search/ui/widgets/search_body.dart
@@ -4,8 +4,10 @@ import 'package:cine_scope/features/bottom_nav/search/logic/search_state.dart';
 import 'package:cine_scope/features/bottom_nav/search/ui/widgets/no_results_view.dart';
 import 'package:cine_scope/features/bottom_nav/search/ui/widgets/search_initial_view.dart';
 import 'package:cine_scope/features/bottom_nav/search/ui/widgets/search_result_item.dart';
+import 'package:cine_scope/features/bottom_nav/search/ui/widgets/search_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
 
 class SearchBody extends StatelessWidget {
   const SearchBody({super.key});
@@ -14,16 +16,16 @@ class SearchBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<SearchCubit, SearchState>(
       builder: (context, state) {
+        final cubit = context.read<SearchCubit>();
+
         /// INITIAL
         if (state.status == RequestsStatus.initial) {
           return const SearchInitialView();
         }
 
-        /// LOADING
-        if (state.status == RequestsStatus.loading) {
-          return const Center(child: CircularProgressIndicator(
-            color: Colors.orange,
-          ));
+        /// LOADING (first search)
+        if (state.status == RequestsStatus.loading && state.results.isEmpty) {
+          return const SearchShimmer();
         }
 
         /// ERROR
@@ -36,25 +38,30 @@ class SearchBody extends StatelessWidget {
           );
         }
 
-        /// SUCCESS
+        /// EMPTY
         if (state.results.isEmpty) {
           return const NoResultsView();
         }
 
-        return ListView.builder(
-          itemCount: state.results.length,
-          itemBuilder: (context, index) {
-            final movie = state.results[index];
-
-            return SearchResultItem(
-              imageUrl: movie.posterPath ?? "",
-              movieId: movie.id,
-              title: movie.title,
-              rating: movie.rating,
-              date: movie.releaseDate,
-              type: movie.type,
-            );
+        return LazyLoadScrollView(
+          onEndOfPage: () {
+            cubit.loadMore();
           },
+          child: ListView.builder(
+            itemCount: state.results.length,
+            itemBuilder: (context, index) {
+              final movie = state.results[index];
+
+              return SearchResultItem(
+                imageUrl: movie.posterPath ?? "",
+                movieId: movie.id,
+                title: movie.title,
+                rating: movie.rating,
+                date: movie.releaseDate,
+                type: movie.type,
+              );
+            },
+          ),
         );
       },
     );
