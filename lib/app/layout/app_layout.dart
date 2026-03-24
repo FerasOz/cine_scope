423b0fe9ef8c1c4b92588bc9ebbe25983c0b22ab
feras.Oz <ferasosama99@gmail.com>
2026-03-24 10:29:31 +0200

    - Editting the bottom nav

---
+++ lib/app/layout/app_layout.dart
@@ -0,0 +1,35 @@
+import 'package:cine_scope/app/layout/widgets/custom_bottom_nav.dart';
+import 'package:cine_scope/app/app_cubit/app_cubit.dart';
+import 'package:cine_scope/app/app_cubit/app_state.dart';
+import 'package:cine_scope/features/home/ui/home_screen.dart';
+import 'package:cine_scope/features/search/ui/search_screen.dart';
+import 'package:cine_scope/features/watch_list/ui/watchlist_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppLayout extends StatelessWidget {
+  const AppLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AppCubit, AppState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          body: state.currentIndex == 0
+              ? const HomeScreen()
+              : state.currentIndex == 1
+              ? const SearchScreen()
+              : const WatchlistScreen(),
+
+          bottomNavigationBar: CustomBottomNav(
+            currentIndex: state.currentIndex,
+            onTap: (index) {
+              context.read<AppCubit>().changeBottomNav(index);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/app/layout/widgets/custom_bottom_nav.dart
@@ -0,0 +1,70 @@
+import 'package:cine_scope/core/helpers/spacing.dart';
+import 'package:cine_scope/core/styles/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+  const CustomBottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 83.h,
+      decoration: BoxDecoration(color: ColorsManager.primary),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildNavItem(
+            icon: Icons.home,
+            label: 'Home',
+            isActive: currentIndex == 0,
+            onTap: () => onTap(0),
+          ),
+          _buildNavItem(
+            icon: Icons.search,
+            label: 'Search',
+            isActive: currentIndex == 1,
+            onTap: () => onTap(1),
+          ),
+          _buildNavItem(
+            icon: Icons.bookmark_outline,
+            label: 'Watch List',
+            isActive: currentIndex == 2,
+            onTap: () => onTap(2),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required IconData icon,
+    required String label,
+    required bool isActive,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: isActive ? Colors.orange : Colors.grey),
+          verticalSpace(4),
+          Text(
+            label,
+            style: TextStyle(
+              color: isActive ? Colors.orange : Colors.grey,
+              fontSize: 12.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
