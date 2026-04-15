f11681dcc28f10dc13bba9e3cddc128ab263b0bd
feras.Oz <ferasosama99@gmail.com>
2026-04-15 13:56:30 +0300

    - Creating auth repo

---
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -0,0 +1,32 @@
+import 'package:cine_scope/core/networking/api_error_handler.dart';
+import 'package:cine_scope/core/networking/api_result.dart';
+import 'package:cine_scope/features/auth/data/data_source/auth_api.dart';
+import 'package:cine_scope/features/auth/data/models/user_model.dart';
+
+class AuthRepo {
+  final AuthApi _api;
+
+  AuthRepo(this._api);
+
+  Future<ApiResult<UserModel>> login(String email, String password) async {
+    try {
+      final response = await _api.login(email, password);
+      return ApiResult.success(UserModel.fromJson(response));
+    } catch (error) {
+      return ApiResult.failure(ApiErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<UserModel>> register(
+    String name,
+    String email,
+    String password,
+  ) async {
+    try {
+      final response = await _api.register(name, email, password);
+      return ApiResult.success(UserModel.fromJson(response));
+    } catch (error) {
+      return ApiResult.failure(ApiErrorHandler.handle(error));
+    }
+  }
+}
