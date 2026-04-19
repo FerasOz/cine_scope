26986d5c05d5fbb107a5e998d41fc75597686eee
feras.Oz <ferasosama99@gmail.com>
2026-04-19 14:14:54 +0300

    - Adding getactivesession & currentUserID

--- lib/features/auth/data/repo/auth_repo.dart
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -1,18 +1,23 @@
 import 'package:cine_scope/core/networking/api_error_handler.dart';
 import 'package:cine_scope/core/networking/api_result.dart';
 import 'package:cine_scope/features/auth/data/data_source/auth_api.dart';
+import 'package:cine_scope/features/auth/data/data_source/auth_session_local_data_source.dart';
 import 'package:cine_scope/features/auth/data/models/user_model.dart';
 
 class AuthRepo {
   final AuthApi _api;
+  final AuthSessionLocalDataSource _sessionLocalDataSource;
 
-  AuthRepo(this._api);
+  AuthRepo(this._api, this._sessionLocalDataSource);
 
-  bool get hasActiveSession => _api.hasActiveSession;
+  bool get hasActiveSession => _sessionLocalDataSource.hasActiveSession;
 
-  Map<String, dynamic>? getActiveSession() => _api.getActiveSession();
+  Map<String, dynamic>? getActiveSession() =>
+      _sessionLocalDataSource.getActiveSession();
 
-  Future<void> clearSession() => _api.clearSession();
+  String? get currentUserId => getActiveSession()?['id']?.toString();
+
+  Future<void> clearSession() => _sessionLocalDataSource.clearSession();
 
   Future<ApiResult<UserModel>> login(String email, String password) async {
     try {
