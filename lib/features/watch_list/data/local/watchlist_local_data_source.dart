156da5b26e1dbb0b8354db7a8e5143602f6027c9
feras.Oz <ferasosama99@gmail.com>
2026-04-19 14:14:15 +0300

    - Making watchlist local for every user

--- lib/features/watch_list/data/local/watchlist_local_data_source.dart
+++ lib/features/watch_list/data/local/watchlist_local_data_source.dart
@@ -6,21 +6,25 @@ class WatchlistLocalDataSource {
 
   WatchlistLocalDataSource(this.box);
 
-  Future<void> addMovie(WatchlistModel movie) async {
-    await box.put(movie.id, movie.toJson());
+  Future<void> addMovie(String userId, WatchlistModel movie) async {
+    await box.put(_movieKey(userId, movie.id), movie.toJson());
   }
 
-  Future<void> removeMovie(int id) async {
-    await box.delete(id);
+  Future<void> removeMovie(String userId, int id) async {
+    await box.delete(_movieKey(userId, id));
   }
 
-  List<WatchlistModel> getMovies() {
-    return box.values
+  List<WatchlistModel> getMovies(String userId) {
+    return box.toMap().entries
+        .where((entry) => entry.key.toString().startsWith('${userId}_'))
+        .map((entry) => entry.value)
         .map((e) => WatchlistModel.fromJson(Map<String, dynamic>.from(e)))
         .toList();
   }
 
-  bool isMovieInWatchlist(int id) {
-    return box.containsKey(id);
+  bool isMovieInWatchlist(String userId, int id) {
+    return box.containsKey(_movieKey(userId, id));
   }
+
+  String _movieKey(String userId, int movieId) => '${userId}_$movieId';
 }
