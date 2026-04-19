de9f260afdbcb9bb10e9caf42f46fdf57139ad65
feras.Oz <ferasosama99@gmail.com>
2026-04-19 14:13:11 +0300

    - Connecting watchlist repo with auth repo

--- lib/features/watch_list/data/repo/watchlist_repo.dart
+++ lib/features/watch_list/data/repo/watchlist_repo.dart
@@ -1,26 +1,37 @@
+import 'package:cine_scope/features/auth/data/repo/auth_repo.dart';
 import 'package:cine_scope/features/watch_list/data/local/watchlist_local_data_source.dart';
 import 'package:cine_scope/features/watch_list/data/models/watchlist_model.dart';
 
 class WatchlistRepo {
   final WatchlistLocalDataSource local;
+  final AuthRepo authRepo;
 
-  WatchlistRepo(this.local);
+  WatchlistRepo(this.local, this.authRepo);
 
   List<WatchlistModel> getWatchlist() {
-    return local.getMovies();
+    final userId = authRepo.currentUserId;
+    if (userId == null) return [];
+
+    return local.getMovies(userId);
   }
 
   Future<void> toggleMovie(WatchlistModel movie) async {
-    final exists = local.isMovieInWatchlist(movie.id);
+    final userId = authRepo.currentUserId;
+    if (userId == null) return;
+
+    final exists = local.isMovieInWatchlist(userId, movie.id);
 
     if (exists) {
-      await local.removeMovie(movie.id);
+      await local.removeMovie(userId, movie.id);
     } else {
-      await local.addMovie(movie);
+      await local.addMovie(userId, movie);
     }
   }
 
   bool isSaved(int id) {
-    return local.isMovieInWatchlist(id);
+    final userId = authRepo.currentUserId;
+    if (userId == null) return false;
+
+    return local.isMovieInWatchlist(userId, id);
   }
 }
